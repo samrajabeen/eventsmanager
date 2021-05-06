770d2144b60f5bf14d7721e768e26f476510e4d5
lenny <llaska@live.com>
2021-05-06 21:47:30 +1000

    added login and signup components, home page has redirect route (needs to be fixed with usestate), next to fix is other pages, then map cards components and admin feature

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,53 @@
 import React from "react";
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route, Link, Redirect} from "react-router-dom";
 import Home from "./pages/Home";
-import Navi from "./components/nav";
 import Events from "./pages/Events";
 import Admin from "./pages/Admin";
 import Contact from "./pages/Contact";
-
+import Login from "./components/login.component";
+import SignUp from "./components/signup.component";
+import "./App.css";
 
 function App() {
-  return  (
-    <>
-    <Navi> Lenard </Navi>
-    <Router>
-      <Route exact path = "/"  component = {Home} />  
-      <Route exact path = "/events" component = {Events} />
-      <Route exact path = "/contact" component = {Contact} />
-      <Route exact path = "/admin" component = {Admin} />
-    </Router>
+  return (<Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+          <Link className="navbar-brand" to={"/sign-in"}>RemoteStack</Link>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-in"}>Sign in</Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to={"/sign-up"}>Sign up</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 
-    </>
-  )
+      <div className="outer">
+        <div className="inner">
+          <Switch>
+            <Route exact path='/' >
+              {isLoggedIn ? <Home /> : 
+              <Redirect to="/sign-in"/> }
+            </Route>
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+            <Route exact path = "/events" component = {Events} />
+            <Route exact path = "/contact" component = {Contact} />
+            <Route exact path = "/admin" component = {Admin} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  );
 }
+
+
+
 
 export default App;
--- models/user.js
+++ models/user.js
@@ -4,11 +4,6 @@ var bcrypt = require('bcryptjs');
 
 // User Schema
 var UserSchema = mongoose.Schema({
-  username: {
-    type: String,
-    index: { unique: true},
-    uppercase: true,
-  },
   password: {
     type: String,
   },
@@ -19,6 +14,7 @@ var UserSchema = mongoose.Schema({
   name: {
     type: String,
     uppercase: true,
+    index: true
   },
   memberType: {
     type: String,
@@ -42,8 +38,8 @@ module.exports.createUser = function(newUser, callback){
   });
 }
 
-module.exports.getUserByUsername = function(username, callback){
-    var query = {username: username};
+module.exports.getUserByEmail = function(email, callback){
+    var query = {email: email};
     User.findOne(query, callback);
   }
   
